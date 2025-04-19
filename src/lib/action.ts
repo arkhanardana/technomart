9bcd11b36f41813aed8182d2beb6f9930265574f
arkhanardana <hanzmantap@gmail.com>
2025-04-19 21:09:18 +0700

    feat: create server action form for sign in

---
+++ src/lib/action.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { ActionResult } from "@/types";
+import { redirect } from "next/navigation";
+import { schemaSignIn } from "./schema";
+import db from "./db";
+import bcrypt from "bcrypt";
+import { lucia } from "./auth";
+import { cookies } from "next/headers";
+
+export async function SignIn(
+  _: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validate = schemaSignIn.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validate.success) {
+    return {
+      error: validate.error.errors[0].message,
+    };
+  }
+
+  const existingUser = await db.user.findFirst({
+    where: {
+      email: validate.data.email,
+    },
+  });
+
+  if (!existingUser) {
+    return {
+      error: "Email not found",
+    };
+  }
+
+  const comparePassword = bcrypt.compareSync(
+    validate.data.password,
+    existingUser.password
+  );
+
+  if (!comparePassword) {
+    return {
+      error: "Email or password incorrect",
+    };
+  }
+
+  const session = await lucia.createSession(existingUser.id, {});
+  const sessionCookie = await lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  redirect("/dashboard");
+}

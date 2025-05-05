56ec086337f861fcb47e5234d16d5bd1ae79dd19
arkhanardana <hanzmantap@gmail.com>
2025-05-05 16:49:02 +0700

    feat: create function for get all categories

---
+++ src/app/(admin)/dashboard/(index)/categories/lib/data.ts
@@ -0,0 +1,13 @@
+import db from "@/lib/db";
+
+export async function getCategories() {
+  try {
+    const categories = await db.category.findMany({});
+
+    return categories;
+  } catch (error) {
+    console.log(error);
+
+    return [];
+  }
+}

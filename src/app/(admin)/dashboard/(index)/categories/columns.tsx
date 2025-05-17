c41b202b0aef898777bef7e63c2d1c81ee92ac36
arkhanardana <hanzmantap@gmail.com>
2025-05-17 13:49:43 +0700

    feat: add alert dialog for delete category

--- src/app/(admin)/dashboard/(index)/categories/columns.tsx
+++ src/app/(admin)/dashboard/(index)/categories/columns.tsx
@@ -4,6 +4,18 @@ import { Button } from "@/components/ui/button";
 import { Category } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { EditIcon, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { deleteCategory } from "./lib/actions";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export const columns: ColumnDef<Category>[] = [
   {
@@ -17,14 +29,41 @@ export const columns: ColumnDef<Category>[] = [
 
       return (
         <div className="flex gap-2">
-          <Button size="sm">
-            <EditIcon className="w-4 h-4" />
-            Edit
+          <Button size="sm" asChild>
+            <Link href={`/dashboard/categories/edit/${category.id}`}>
+              <EditIcon className="w-4 h-4" />
+              Edit
+            </Link>
           </Button>
-          <Button size="sm" variant={"destructive"}>
-            <Trash2 className="w-4 h-4" />
-            Delete
-          </Button>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button size="sm" variant="destructive">
+                <Trash2 className="w-4 h-4" />
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <h2 className="text-lg font-medium">
+                  Are you sure you want to delete this category?
+                </h2>
+              </AlertDialogHeader>
+
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                category.
+              </AlertDialogDescription>
+
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => deleteCategory(category.id)}>
+                  Yes
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
       );
     },

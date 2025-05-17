cae384363dc497adbfa1e09ec78e6b451ba19fdf
arkhanardana <hanzmantap@gmail.com>
2025-05-17 13:49:29 +0700

    feat: create action form for update and post category

---
+++ src/app/(admin)/dashboard/(index)/categories/lib/actions.ts
@@ -0,0 +1,76 @@
+"use server";
+
+import db from "@/lib/db";
+import { schemaCategory } from "@/lib/schema";
+import { ActionResult } from "@/types";
+import { redirect } from "next/navigation";
+
+export async function postCategory(
+  _: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validated = schemaCategory.safeParse({ name: formData.get("name") });
+
+  if (!validated.success) {
+    return {
+      error: validated.error.errors[0].message,
+    };
+  }
+  try {
+    await db.category.create({ data: { name: validated.data.name } });
+  } catch (error) {
+    return {
+      error: "Failed to update category",
+    };
+  }
+
+  return redirect("/dashboard/categories");
+}
+
+export async function updateCategory(
+  _: unknown,
+  formData: FormData,
+  id: number | undefined
+): Promise<ActionResult> {
+  const validated = schemaCategory.safeParse({ name: formData.get("name") });
+
+  if (!validated.success) {
+    return {
+      error: validated.error.errors[0].message,
+    };
+  }
+
+  if (id === undefined) {
+    return {
+      error: "Category not found",
+    };
+  }
+
+  try {
+    await db.category.update({
+      where: { id },
+      data: { name: validated.data.name },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update category",
+    };
+  }
+  return redirect("/dashboard/categories/");
+}
+
+export async function deleteCategory(id: number) {
+  try {
+    await db.category.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete category",
+    };
+  }
+
+  return redirect("/dashboard/categories");
+}

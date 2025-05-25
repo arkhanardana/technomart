356d569123ab3975dfbac87324808c3792abaca9
arkhanardana <hanzmantap@gmail.com>
2025-05-25 15:09:21 +0700

    feat: add image schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+export const ALLOW_MIME_TYPES = ["image/jpeg", "image/png", "image/jpg"];
+
 export const schemaSignIn = z.object({
   email: z.string({ required_error: "Email is required" }).email({
     message: "Please input a valid email",
@@ -19,4 +21,18 @@ export const schemaLocation = z.object({
   name: z
     .string({ required_error: "Location name is required" })
     .min(4, { message: "Location name should have min 4 characters" }),
+});
+
+export const schemaBrand = z.object({
+  name: z
+    .string({ required_error: "Brand name is required" })
+    .min(4, { message: "Brand name should have min 4 characters" }),
+  image: z
+    .any()
+    .refine((file: File) => ALLOW_MIME_TYPES.includes(file.type), {
+      message: "Only .jpg, .jpeg and .png formats are allowed",
+    })
+    .refine((file: File) => file?.name, {
+      message: "Image is required",
+    }),
 });

517c35b030da1e943cdd17217e0f947ec13bf45b
arkhanardana <hanzmantap@gmail.com>
2025-05-25 15:11:10 +0700

    feat: create page for create

---
+++ src/app/(admin)/dashboard/(index)/brands/create/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FormBrand from "../_components/form-brand";
+
+export default function CreateBrandPage() {
+  return (
+    <div>
+      <FormBrand />
+    </div>
+  );
+}

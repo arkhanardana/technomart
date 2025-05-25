b3cbc94869541244547413f25781284f3dfe04de
arkhanardana <hanzmantap@gmail.com>
2025-05-25 15:12:20 +0700

    feat: add columns for data table brands

---
+++ src/app/(admin)/dashboard/(index)/brands/columns.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Brand } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, Trash2 } from "lucide-react";
+import Link from "next/link";
+// import { deleteLocation } from "./lib/actions";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { getImageUrl } from "@/lib/supabase";
+
+export const columns: ColumnDef<Brand>[] = [
+  {
+    accessorKey: "name",
+    header: "Brand name",
+    cell: ({ row }) => {
+      const brand = row.original;
+
+      return (
+        <div className="inline-flex items-center gap-5">
+          <Image
+            src={getImageUrl(brand.logo)}
+            alt={brand.name}
+            width={80}
+            height={80}
+          />
+          <span>{brand.name}</span>
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const brand = row.original;
+
+      return (
+        <div className="flex gap-2">
+          <Button size="sm" asChild>
+            <Link href={`/dashboard/brands/edit/${brand.id}`}>
+              <EditIcon className="w-4 h-4" />
+              Edit
+            </Link>
+          </Button>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button size="sm" variant="destructive">
+                <Trash2 className="w-4 h-4" />
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <h2 className="text-lg font-medium">
+                  Are you sure you want to delete this location?
+                </h2>
+              </AlertDialogHeader>
+
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                location.
+              </AlertDialogDescription>
+
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Yes</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      );
+    },
+  },
+];

9415a0bb1abd1e6cf10fd6c7b461bb62228d6fe2
arkhanardana <hanzmantap@gmail.com>
2025-05-31 13:49:05 +0700

    feat: create products page

--- src/app/(admin)/dashboard/(index)/products/page.tsx
+++ src/app/(admin)/dashboard/(index)/products/page.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+import { columns } from "./columns";
+import { getAllProducts } from "./lib/data";
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const products = await getAllProducts();
+
   return (
-    <div>
-      <h1>Products Page</h1>
+    <div className="space-y-4">
+      <div className="text-right">
+        <Button size="sm" className="h-8 gap-1" asChild>
+          <Link href="/dashboard/products/create">
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Add product
+            </span>
+          </Link>
+        </Button>
+      </div>
+      <Card x-chunk="dashboard-06-chunk-0">
+        <CardHeader>
+          <CardTitle>Products</CardTitle>
+          <CardDescription>
+            Manage your products and view their sales performance.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={products} />
+        </CardContent>
+      </Card>
     </div>
   );
 }

b128d1732c9265624aefe3367181f54293c0d354
arkhanardana <hanzmantap@gmail.com>
2025-06-02 13:33:35 +0700

    feat: add trim for every string schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -3,29 +3,33 @@ import { z } from "zod";
 export const ALLOW_MIME_TYPES = ["image/jpeg", "image/png", "image/jpg"];
 
 export const schemaSignIn = z.object({
-  email: z.string({ required_error: "Email is required" }).email({
+  email: z.string({ required_error: "Email is required" }).trim().email({
     message: "Please input a valid email",
   }),
   password: z
     .string({ required_error: "Password is required" })
+    .trim()
     .min(5, { message: "Password should have min 5 characters" }),
 });
 
 export const schemaCategory = z.object({
   name: z
     .string({ required_error: "Category name is required" })
+    .trim()
     .min(4, { message: "Category name should have min 4 characters" }),
 });
 
 export const schemaLocation = z.object({
   name: z
     .string({ required_error: "Location name is required" })
+    .trim()
     .min(4, { message: "Location name should have min 4 characters" }),
 });
 
 export const schemaBrand = z.object({
   name: z
     .string({ required_error: "Brand name is required" })
+    .trim()
     .min(4, { message: "Brand name should have min 4 characters" }),
   image: z
     .any()
@@ -38,7 +42,7 @@ export const schemaBrand = z.object({
 });
 
 export const schemaUpdateBrand = z.object({
-  name: z.string({ required_error: "Brand name is required" }).min(4, {
+  name: z.string({ required_error: "Brand name is required" }).trim().min(4, {
     message: "Brand name should have min 4 characters",
   }),
   image: z
@@ -52,4 +56,19 @@ export const schemaUpdateBrand = z.object({
       }),
     ])
     .optional(),
+});
+
+export const schemaProduct = z.object({
+  name: z
+    .string({ required_error: "Category name is required" })
+    .trim()
+    .min(4, { message: "Category name should have min 4 characters" }),
+  description: z
+    .string({ required_error: "Description is required" })
+    .trim()
+    .min(10, { message: "Description should have min 10 characters" }),
+  price: z.string({ required_error: "Price is required" }),
+  stock: z.string({ required_error: "Stock is required" }),
+  category_id: z.string({ required_error: "Category is required" }),
+  brand_id: z.string({ required_error: "Brand is required" }),
 });

0a163ad152701072edcb290ca96b35919cccb9bd
arkhanardana <hanzmantap@gmail.com>
2025-06-04 10:49:12 +0700

    feat: create action for update product

--- src/app/(admin)/dashboard/(index)/products/lib/actions.ts
+++ src/app/(admin)/dashboard/(index)/products/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import db from "@/lib/db";
-import { schemaProduct } from "@/lib/schema";
+import { schemaProduct, schemaProductEdit } from "@/lib/schema";
 import { uploadImage } from "@/lib/supabase";
 import { ActionResult } from "@/types";
 import { StockProduct } from "@prisma/client";
@@ -53,6 +53,90 @@ export async function postProducts(
 
     return {
       error: "Failed to post product",
+    };
+  }
+
+  return redirect("/dashboard/products");
+}
+
+export async function updateProducts(
+  _: unknown,
+  formData: FormData,
+  id: number
+): Promise<ActionResult> {
+  const validated = schemaProductEdit.safeParse({
+    name: formData.get("name"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+    brand_id: formData.get("brand_id"),
+    category_id: formData.get("category_id"),
+    location_id: formData.get("location_id"),
+    stock: formData.get("stock"),
+    id: id,
+  });
+
+  if (!validated.success) {
+    return {
+      error: validated.error.errors[0].message,
+    };
+  }
+
+  const product = await db.product.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  if (!product) {
+    return {
+      error: "Product not found",
+    };
+  }
+
+  const uploadedImages = formData.getAll("images") as File[];
+  let filenames = [];
+
+  if (uploadedImages.length === 3) {
+    const parseImages = schemaProduct.pick({ images: true }).safeParse({
+      images: uploadedImages,
+    });
+
+    if (!parseImages.success) {
+      return {
+        error: parseImages.error.errors[0].message,
+      };
+    }
+
+    for (const image of uploadedImages) {
+      const fileName = await uploadImage(image, "products");
+      filenames.push(fileName);
+    }
+  } else {
+    filenames = product.images;
+  }
+
+  try {
+    await db.product.update({
+      where: {
+        id,
+      },
+
+      data: {
+        name: validated.data.name,
+        price: Number.parseInt(validated.data.price),
+        description: validated.data.description,
+        brand_id: Number.parseInt(validated.data.brand_id),
+        category_id: Number.parseInt(validated.data.category_id),
+        location_id: Number.parseInt(validated.data.location_id),
+        stock: validated.data.stock as StockProduct,
+        images: filenames,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return {
+      error: "Failed to update product",
     };
   }
 

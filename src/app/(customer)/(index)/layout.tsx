84516fe5ce6bb1038c1e81e1b59ee901c5a61f9c
arkhanardana <hanzmantap@gmail.com>
2025-06-05 12:08:12 +0700

    feat: create layout for customer landing page

---
+++ src/app/(customer)/(index)/layout.tsx
@@ -0,0 +1,15 @@
+import { Poppins } from "next/font/google";
+import { ReactNode } from "react";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "700", "800"],
+  subsets: ["latin-ext"],
+});
+
+export default function CustomerLayout({ children }: { children: ReactNode }) {
+  return (
+    <main>
+      <div className={poppins.className}>{children}</div>
+    </main>
+  );
+}

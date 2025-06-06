6a3b5f5f430e10cae99dbff19296b246a62c5323
arkhanardana <hanzmantap@gmail.com>
2025-06-06 13:43:30 +0700

    feat: add categories to list

--- src/app/(admin)/dashboard/(index)/categories/page.tsx
+++ src/app/(admin)/dashboard/(index)/categories/page.tsx
@@ -15,6 +15,7 @@ import { getCategories } from "./lib/data";
 
 export default async function CategoriesPage() {
   const categories = await getCategories();
+
   return (
     <div className="space-y-4">
       <div className="text-right">

299a16e012a2dbac66a94a60a8c60ea897387c4d
arkhanardana <hanzmantap@gmail.com>
2025-06-08 14:51:28 +0700

    feat: add the components to catalog page

---
+++ src/app/(customer)/(index)/catalogs/page.tsx
@@ -0,0 +1,42 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+import SearchBar from "./_components/search-bar";
+import FilterPrice from "./_components/filter-price";
+import FilterBrands from "./_components/filter-brands";
+import FilterLocation from "./_components/filter-location";
+import FilterCategory from "./_components/filter-category";
+
+export default function CatalogsPage() {
+  return (
+    <div>
+      <header className="bg-[#EFF3FA] pt-[30px] h-[351px] -mb-[181px]">
+        <Navbar />
+      </header>
+      <SearchBar />
+      <div
+        id="catalog"
+        className="container max-w-[1130px] mx-auto flex gap-[30px] mt-[50px] pb-[100px]"
+      >
+        <form
+          action=""
+          className="flex flex-1 flex-col bg-white p-[30px] gap-5 h-fit border border-[#E5E5E5] rounded-[30px]"
+        >
+          <h2 className="font-bold text-2xl leading-[34px]">Filters</h2>
+          <FilterPrice />
+          <hr className="border-[#E5E5E5]" />
+          {/* <FilterStock /> */}
+          <hr className="border-[#E5E5E5]" />
+          <FilterBrands />
+          <hr className="border-[#E5E5E5]" />
+          <FilterLocation />
+          <hr className="border-[#E5E5E5]" />
+          <FilterCategory />
+        </form>
+        <div className="w-[780px] flex flex-col bg-white p-[30px] gap-[30px] h-fit border border-[#E5E5E5] rounded-[30px]">
+          <h2 className="font-bold text-2xl leading-[34px]">Products</h2>
+          {/* <ProductListing /> */}
+        </div>
+      </div>
+    </div>
+  );
+}

27b863d7217992f3f61fb29c187ed453bb4e9d29
arkhanardana <hanzmantap@gmail.com>
2025-06-08 14:50:47 +0700

    feat: create filter components

---
+++ src/app/(customer)/(index)/catalogs/_components/filter-category.tsx
@@ -0,0 +1,33 @@
+import { getCategories } from "@/app/(admin)/dashboard/(index)/categories/lib/data";
+import React from "react";
+
+export default async function FilterCategory() {
+  const categories = await getCategories();
+
+  return (
+    <div className="flex flex-col gap-[14px]">
+      <p className="font-semibold leading-[22px]">Categories</p>
+      {categories.map((item) => (
+        <label
+          key={`${item.id + item.name}`}
+          htmlFor={`${item.id + item.name}`}
+          className="font-semibold flex items-center gap-3"
+        >
+          <input
+            type="checkbox"
+            name="brand"
+            value={item.id}
+            className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
+          />
+          <span>{item.name}</span>
+        </label>
+        // <FilterCheckboxItem
+        //   type="brand"
+        //   key={item.id + item.name}
+        //   id={item.id.toString()}
+        //   value={item.name}
+        // />
+      ))}
+    </div>
+  );
+}

77cc9421b09c8d8eb0901dc931c817e7275af48a
arkhanardana <hanzmantap@gmail.com>
2025-06-08 22:54:00 +0700

    feat: add filter checkbox to filter components

--- src/app/(customer)/(index)/catalogs/_components/filter-stock.tsx
+++ src/app/(customer)/(index)/catalogs/_components/filter-stock.tsx
@@ -1,25 +1,12 @@
+import FilterCheckbox from "./filter-checkbox";
+
 export default function FilterStock() {
   return (
     <div className="flex flex-col gap-[14px]">
       <p className="font-semibold leading-[22px]">Stocks</p>
-      {/* <label className="font-semibold flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      name="stock"
-                      className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-                    />
-                    <span>Pre Order</span>
-                </label>
-                <label className="font-semibold flex items-center gap-3">
-                    <input
-                      type="checkbox"
-                      name="stock"
-                      className="w-6 h-6 flex shrink-0 appearance-none checked:border-[3px] checked:border-solid checked:border-white rounded-md checked:bg-[#0D5CD7] ring-1 ring-[#0D5CD7]"
-                    />
-                    <span>Ready Stock</span>
-                </label> */}
-      {/* <FilterCheckboxItem type="stock" id={"ready"} value="Ready" /> */}
-      {/* <FilterCheckboxItem type="stock" id={"preorder"} value="Pre-order" /> */}
+
+      <FilterCheckbox id={"ready"} value="Ready" type="stock" />
+      <FilterCheckbox id={"preorder"} value="Pre-order" type="stock" />
     </div>
   );
 }

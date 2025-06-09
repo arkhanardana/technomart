2eb589a32a6a51b2b393842e99dc7eb8fb57bdea
arkhanardana <hanzmantap@gmail.com>
2025-06-09 13:55:42 +0700

    feat: add card product

--- src/app/(customer)/(index)/catalogs/_components/product-listing.tsx
+++ src/app/(customer)/(index)/catalogs/_components/product-listing.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { fetchProducts } from "../lib/data";
+import CardProduct from "../../_components/card-product";
 
 export default function ProductListing() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["product-listing"],
+    queryFn: () => fetchProducts(),
+  });
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 gap-[30px]">
+        <span>Loading...</span>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Product Listing</h1>
+    <div className="grid grid-cols-3 gap-[30px]">
+      {data?.map((product) => <CardProduct key={product.id} item={product} />)}
     </div>
   );
 }

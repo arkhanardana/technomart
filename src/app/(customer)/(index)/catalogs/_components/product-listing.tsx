10cc6ee99806dc9de00e417669284424f187cde4
arkhanardana <hanzmantap@gmail.com>
2025-06-09 19:43:50 +0700

    feat: add filter to use query data

--- src/app/(customer)/(index)/catalogs/_components/product-listing.tsx
+++ src/app/(customer)/(index)/catalogs/_components/product-listing.tsx
@@ -4,11 +4,13 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { fetchProducts } from "../lib/data";
 import CardProduct from "../../_components/card-product";
+import { useFilter } from "@/hooks/useFilter";
 
 export default function ProductListing() {
+  const { filter } = useFilter();
   const { data, isLoading } = useQuery({
-    queryKey: ["product-listing"],
-    queryFn: () => fetchProducts(),
+    queryKey: ["product-listing", filter],
+    queryFn: () => fetchProducts(filter),
   });
 
   if (isLoading) {
@@ -19,9 +21,21 @@ export default function ProductListing() {
     );
   }
 
+  if (!data || data.length === 0) {
+    return (
+      <div className="grid grid-cols-1 gap-[30px]">
+        <span>Not found</span>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-3 gap-[30px]">
-      {data?.map((product) => <CardProduct key={product.id} item={product} />)}
+      {data.map((product) => (
+        <div className="" key={product.id}>
+          <CardProduct item={product} />
+        </div>
+      ))}
     </div>
   );
 }

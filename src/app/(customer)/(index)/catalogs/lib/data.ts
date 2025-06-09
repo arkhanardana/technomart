855eaf708a24e78a885f3298ed8c1df7ca93807e
arkhanardana <hanzmantap@gmail.com>
2025-06-09 13:57:36 +0700

    feat: add try catch

--- src/app/(customer)/(index)/catalogs/lib/data.ts
+++ src/app/(customer)/(index)/catalogs/lib/data.ts
@@ -2,12 +2,17 @@ import { TFilter } from "@/hooks/useFilter";
 import { TProduct } from "@/types";
 
 export async function fetchProducts(body?: TFilter): Promise<TProduct[]> {
-  const res = await fetch("/api/catalog", {
-    method: "POST",
-    body: JSON.stringify(body ?? {}),
-  });
+  try {
+    const res = await fetch("/api/catalog", {
+      method: "POST",
+      body: JSON.stringify(body ?? {}),
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  return data ?? [];
+    return data ?? [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }

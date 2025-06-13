989526f420f6721b0dd2c8055c7190cfa6565409
arkhanardana <hanzmantap@gmail.com>
2025-06-13 19:23:08 +0700

    feat: add shipping address schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -105,3 +105,22 @@ export const schemaProductEdit = schemaProduct
     id: z.number({ required_error: "Id is required" }),
   })
   .omit({ images: true });
+
+export const schemaShippingAddress = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(5, { message: "Name should have 5 minimal characters" }),
+  address: z
+    .string({ required_error: "Address is required" })
+    .min(5, { message: "Address should have 5 minimal characters" }),
+  city: z
+    .string({ required_error: "City is required" })
+    .min(5, { message: "City should have 5 minimal characters" }),
+  postal_code: z
+    .string({ required_error: "Postal Code is required" })
+    .min(5, { message: "Postal Code should have 5 minimal characters" }),
+  notes: z.string().nullable(),
+  phone: z
+    .string({ required_error: "Phone is required" })
+    .min(5, { message: "Phone should have 5 minimal characters" }),
+});
